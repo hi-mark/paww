c59088cdc38d704a0b8d5a64e98ef38a85e5c2f2
hi-mark <44605927+hi-mark@users.noreply.github.com>
2025-05-12 17:38:48 -0500

    improved cn to filter out undefined/null

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,5 @@
-export const cn = (...classes: any) => {
-  return classes.join(" ");
+export const cn = (
+  ...classes: (string | false | null | undefined)[]
+): string => {
+  return classes.filter(Boolean).join(" ");
 };
-
-export async function fetchFromAPI<T>(
-  path: string,
-  options: RequestInit = {}
-): Promise<T> {
-  const res = await fetch(`/api/proxy/${path}`, {
-    credentials: "include",
-    ...options,
-  });
-
-  if (!res.ok) {
-    const message = await res.text();
-    throw new Error(message || "API request failed");
-  }
-
-  return res.json();
-}
